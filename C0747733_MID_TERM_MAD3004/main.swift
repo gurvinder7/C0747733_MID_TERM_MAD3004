80dfed5c1e63fd2e409b0f7faac9e059ada2ded8
gurvinder mangat <gurvindermangat@gurvinders-mbp>
2019-02-20 18:56:34 -0500

    Initial Commit

---
+++ C0747733_MID_TERM_MAD3004/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C0747733_MID_TERM_MAD3004
+//
+//  Created by gurvinder mangat on 2019-02-20.
+//  Copyright © 2019 gurvinder mangat. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
